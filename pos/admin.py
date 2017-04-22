79bd6460671a72bd01dc420ac6ef9a2af4eda1ed
Rafael Correa <rafael_correa_3@hotmail.com>
2017-04-22 14:22:47 -0500

    admin.py

--- pos/admin.py
+++ pos/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Employee, Store
-
+from .models import (
+    Employee,
+    Store,
+    Order,
+    Supplier,
+    Product,
+    StoreProduct,
+    OrderProduct
+)
 # Register your models here.
 admin.site.register(Store)
 admin.site.register(Employee)
-
+admin.site.register(Order)
+admin.site.register(Supplier)
+admin.site.register(Product)
+admin.site.register(StoreProduct)
+admin.site.register(OrderProduct)
